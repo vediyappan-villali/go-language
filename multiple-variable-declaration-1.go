3f2e9d72e48644802121f8727585eba29d4e9dd2
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-06 18:52:02 +0530

    Add program to declare multiple variable of similar type in a line
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ multiple-variable-declaration-1.go
@@ -0,0 +1,13 @@
+// Go program to understand multiple variable decalartion
+package main
+
+import "fmt"
+
+func main() {
+    var num1, num2, num3, num4 int = 10, 11, 15, 16
+
+    fmt.Println("num1 = ", num1)
+    fmt.Println("num2 = ", num2)
+    fmt.Println("num3 = ", num3)
+    fmt.Println("num4 = ", num4)
+}
