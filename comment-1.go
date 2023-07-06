d560904f4831faf838f2d654a1e647c263367572
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-06 19:01:07 +0530

    Rename file
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ comment-1.go
@@ -0,0 +1,13 @@
+// Go program to use single line comment
+
+package main
+
+import ( "fmt" )
+
+func main() {
+    fmt.Println("Hello world!")
+    // Single line comment
+    // fmt.Println("This line is commented! Uncomment to print it!")
+    // fmt.Println("This line also commented! Uncomment to print it!")
+    // fmt.Println("This line too commented! Uncomment to print it!")
+}
---
+++ comment-2.go
@@ -0,0 +1,15 @@
+// Go program to use multiple line comment
+
+package main
+
+import ( "fmt" )
+
+func main() {
+    fmt.Println("Hello world!")
+    // Multiple line comment
+    /* 
+    fmt.Println("This line is commented! Uncomment to print it!")
+    fmt.Println("This line also commented! Uncomment to print it!")
+    fmt.Println("This line too commented! Uncomment to print it!")
+    */
+}
