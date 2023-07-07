15f10c5e45f8bdecef5500208530ce22359452d1
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-07 09:02:14 +0530

    Add program to understand naming variables
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ naming-rules.go
@@ -0,0 +1,15 @@
+// Go program to understand naming variables
+
+package main
+
+import "fmt"
+
+func main() {
+    var myCountryName = "India"
+    var MyCountryName = "India"
+    var my_country_name = "India"
+
+    fmt.Println("myCountryName = ", myCountryName)
+    fmt.Println("MyCountryName = ", MyCountryName)
+    fmt.Println("my_country_name = ", my_country_name)
+}
