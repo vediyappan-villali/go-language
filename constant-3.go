bccdf33ac815a1251dfd0a7c10f6308044743ef6
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-08 10:34:08 +0530

    Add programt to declare multiple constant
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ constant-3.go
@@ -0,0 +1,21 @@
+// Go program to declare multiple constant
+
+package main
+
+import "fmt"
+
+const (
+    MAX int = 10
+    PI = 3.14
+    NAME = "Vediyappan"
+)
+
+func main() {
+    // MAX = 20
+    // PI = 5.14
+    // NAME = "John"
+
+    fmt.Println("MAX = ", MAX)
+    fmt.Println("PI = ", PI)
+    fmt.Println("NAME = ", NAME)
+}
