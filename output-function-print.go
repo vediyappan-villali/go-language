63d080b872bcf626eb2f93bac504ffa9043262ba
vediyappan-hcl <vediyappan.villali@hcl.com>
2023-07-10 15:30:20 +0530

    Add program to understand output function - print
    Signed-off-by: vediyappan-hcl <vediyappan.villali@hcl.com>

---
+++ output-function-print.go
@@ -0,0 +1,17 @@
+// Go program to understand output function - Print
+
+package main
+
+import "fmt"
+
+func main() {
+    var str1, str2 string = "Vediyappan", "Villali"
+    var num1, num2 = 10, 20
+
+    // fmt.Print(str1, str2)
+    // fmt.Print(str1, "\n", str2, "\n")
+    fmt.Print(str1, " ", str2, "\n")
+
+    // fmt.Print(num1, num2)
+    fmt.Print(num1, num2, "\n")
+}
