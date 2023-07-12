ebf4cd2b9245aa9d4d30e23e70cbb8187c62bb35
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-12 08:40:41 +0530

    Add program to understand Printf - float type
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ output-function-printf-float.go
@@ -0,0 +1,15 @@
+// Go program to understand output function - Printf
+
+package main
+
+import "fmt"
+
+func main() {
+    var pi = 3.141
+
+    fmt.Printf("%e\n", pi)
+    fmt.Printf("%f\n", pi)
+    fmt.Printf("%.2f\n", pi)
+    fmt.Printf("%6.2f\n", pi)
+    fmt.Printf("%g\n", pi)
+} 
