fbd08df97f4ebf2e000aa34af7188ab275fa13ca
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-12 08:40:21 +0530

    Add program to understand Printf - boolean type
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ output-function-printf-boolean.go
@@ -0,0 +1,13 @@
+// Go program to understand output function - Printf
+
+package main
+
+import "fmt"
+
+func main() {
+    var status = true
+    var flag   = false
+
+    fmt.Printf("status = %t\n", status)
+    fmt.Printf("flag = %t\n", flag)
+}
