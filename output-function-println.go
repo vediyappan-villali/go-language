2ce46ced56d651229a58437b0926014ba11c7955
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-15 08:29:11 +0530

    Add program to understand output function - Println
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ output-function-println.go
@@ -0,0 +1,11 @@
+// Go program to understand output function - Println
+
+package main
+
+import "fmt"
+
+func main() {
+    var str1, str2 = "Vediyappan", "Villali"
+
+    fmt.Println(str1, str2)
+}
