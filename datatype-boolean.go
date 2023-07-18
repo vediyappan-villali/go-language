33c7f50c473e5716e22b21033a88b95eb17ee245
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-18 09:02:06 +0530

    Add program to understand boolean datatype
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ datatype-boolean.go
@@ -0,0 +1,17 @@
+// Go program to understand boolean datatype
+
+package main
+
+import "fmt"
+
+func main() {
+    var status1 bool
+    var status2 bool = true
+    var status3 = true
+    status4 := true
+
+    fmt.Println(status1)
+    fmt.Println(status2)
+    fmt.Println(status3)
+    fmt.Println(status4)
+}
