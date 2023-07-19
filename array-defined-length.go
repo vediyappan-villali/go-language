4af8c078fe79104c7ef204f37987ce8311e146c7
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-19 09:00:38 +0530

    Add program to understand array with defined length
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ array-defined-length.go
@@ -0,0 +1,13 @@
+// Go program to understand array
+
+package main
+
+import "fmt"
+
+func main() {
+    var arr1 = [5]int{10, 20, 30, 40, 50}
+    arr2 := [3]int{100, 200, 300}
+
+    fmt.Println(arr1)
+    fmt.Println(arr2)
+}
