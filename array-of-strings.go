5cf63a219de44b03f6837555ce7683314f7adf7f
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-19 09:01:30 +0530

    Add program to understand array of strings
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ array-of-strings.go
@@ -0,0 +1,11 @@
+// Go program to understand array of strings
+
+package main
+
+import "fmt"
+
+func main() {
+    var arr = []string{"Apple", "Samsung", "Nokia", "Moto"}
+
+    fmt.Println(arr)
+}
