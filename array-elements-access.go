bc288d4482940d7742a34e3fc431beaa98f192af
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-20 08:42:52 +0530

    Add program to access array elements
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ array-elements-access.go
@@ -0,0 +1,13 @@
+// Go program to access array elements
+
+package main
+
+import "fmt"
+
+func main() {
+    var arr = [3]int{10, 20, 30}
+
+    fmt.Println(arr[0])
+    fmt.Println(arr[1])
+    fmt.Println(arr[2])
+}
