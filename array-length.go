a344a673fabe284c887b61dece85684981645cae
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-21 21:48:09 +0530

    Add program to get array length
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ array-length.go
@@ -0,0 +1,13 @@
+// Go program to understand array length
+
+package main
+
+import "fmt"
+
+func main() {
+    var cars = []string{"Maruti", "Tata", "Jaguar", "Nissan"}
+    var nums = []int{1, 2, 3, 4, 5, 6}
+
+    fmt.Println(len(cars))
+    fmt.Println(len(nums))
+}
