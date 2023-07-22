33fc3c23b915ed1ade4cbf23213569b19c43014c
vediyappan-villali <vediyappan.villali@gmail.com>
2023-07-22 13:56:58 +0530

    Add program to understand slice
    Signed-off-by: vediyappan-villali <vediyappan.villali@gmail.com>

---
+++ slice-1.go
@@ -0,0 +1,13 @@
+// Go program to understand slice
+
+package main
+
+import "fmt"
+
+func main() {
+    var arr = []int{10, 20, 30, 40, 50, 60}
+
+    var slc = arr[1:4]
+
+    fmt.Println(slc) 
+}
